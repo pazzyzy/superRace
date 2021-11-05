f5215939024fb90fa44471fba811139e35d0aef3
pazzyzy <goodzzone@gmail.com>
2021-11-05 10:54:00 +0300

    no message

--- 27.09_SuperRace/ViewControllerSettings.swift
+++ 27.09_SuperRace/ViewControllerSettings.swift
@@ -15,17 +15,33 @@ class ViewControllerSettings: UIViewController {
     @IBOutlet weak var pointsScreen: UIImageView!
     @IBOutlet weak var classicCarImage: UIImageView!
     @IBOutlet weak var formula1CarImage: UIImageView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var accessClosed: UILabel!
     
     var x:Bool = true
     var y:Bool = true
+    var checkAge:Int = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        config()
         setupCar()
         print(NSHomeDirectory())
         
     }
+    
+    func config() {
+        configDatePicker()
+    }
+    
+    func configDatePicker() {
+        
+    }
+    
     func updateView() {
         classicCarImage.isHidden = (selectCar.selectedSegmentIndex == 1)
         formula1CarImage.isHidden = (selectCar.selectedSegmentIndex == 0)
@@ -87,16 +103,6 @@ class ViewControllerSettings: UIViewController {
         writePlayerDifficulty()
     }
     
-    @IBAction func actionStartRace(_ sender: Any) {
-        
-        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations:
-                        { self.pointsScreen.alpha = 0
-                            self.pointsScreen.transform = CGAffineTransform(scaleX: 2, y: 2)
-                        },
-                       completion: {_ in self.nextPage()})
-        writeSettings()
-    }
-    
     func animatedScreen() {
         UIView.animate(withDuration: 1, delay: 0, options: [], animations:
                         { self.pointsScreen.alpha = 0.2
@@ -119,5 +125,39 @@ class ViewControllerSettings: UIViewController {
             navigationController?.pushViewController(controller, animated: true)
         }
         writePlayerName()
+    }
+    
+    func goToNextPage() {
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations:
+                        { self.pointsScreen.alpha = 0
+                            self.pointsScreen.transform = CGAffineTransform(scaleX: 2, y: 2)
+                        },
+                       completion: {_ in self.nextPage()})
+        writeSettings()
+    }
+    
+    @IBAction func actionStartRace(_ sender: Any) {
+        
+        goToNextPage()
+    }
+    
+    @IBAction func didSelectData(_ sender: Any) {
+        let dataFormatter = DateFormatter()
+        dataFormatter.dateFormat = "YYYY"
+        let dateString = dataFormatter.string(from: datePicker.date)
+        guard let age = Int(dateString) else {
+            return
+        }
+        let checkAge = 2021 - age
+        if checkAge >= 18 {
+            print(checkAge)
+            startButton.isHidden = false
+            accessClosed.isHidden = true
+        } else {
+            print(checkAge)
+            startButton.isHidden = true
+            accessClosed.isHidden = false
+        }
     }
 }
